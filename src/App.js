b77cad633f074e47cec8be6336e23e8e2c482b2e
Elijahscriptdev <elijahobominuru@gmail.com>
2021-01-23 12:52:34 +0100

    stylyed request details page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import RequestInformation from './components/request/RequestInformation';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <RequestInformation />
     </div>
   );
 }
---
+++ src/components/request/RequestInformation.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./RequestInformation.css";
+import { RiArrowDownSLine } from "react-icons/ri";
+
+const RequestInformation = () => {
+  return (
+    <div className='request-info-container'>
+      <div className='request-info-title'>Request Information</div>
+      <div className='request-info-datas'>
+        <div className='request-info-data'>
+          <div className='data-one'>
+            <h3>Request Type</h3>
+            <p>Post-Con</p>
+          </div>
+          <div className='data-one'>
+            <h3>Customer Name</h3>
+            <p>Peter Abu-Ekpeshie</p>
+          </div>
+          <div className='data-one'>
+            <h3>Cleaning Date</h3>
+            <p>
+              09<span>&#8226;</span>10<span>&#8226;</span>2019
+            </p>
+          </div>
+        </div>
+
+        <div className='request-info-data'>
+          <div className='data-one'>
+            <h3>Invoice Status</h3>
+            <p className='special'>Ungenerated</p>
+          </div>
+          <div className='data-one'>
+            <h3>Request Date</h3>
+            <p>
+              09<span>&#8226;</span>10<span>&#8226;</span>2019
+            </p>
+          </div>
+          <div className='data-one'>
+            <h3>Request Time</h3>
+            <p>8:00 A.M</p>
+          </div>
+        </div>
+
+        <div className='request-info-data'>
+          <div className='data-one'>
+            <h3>Location</h3>
+            <p>
+              8, Yovi street, <br />
+              Iwaya, Onike, Yaba.
+            </p>
+          </div>
+          {/* <div className="data-one">
+                <h3>Request Type</h3>
+                <p>Post-Con</p>
+            </div> */}
+          <div className='data-one'>
+            <h3>House-Type</h3>
+            <p>5-Bedroom Duplex</p>
+          </div>
+        </div>
+
+        <div>
+          <hr />
+        </div>
+
+        <div className='request-info-action'>
+          <span className='first'>Action</span>
+          <span className='second'>
+            <RiArrowDownSLine />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RequestInformation;

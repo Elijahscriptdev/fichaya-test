d8320bafcfe451e21045708a16a36f388d21fce2
Elijahscriptdev <elijahobominuru@gmail.com>
2021-01-23 23:39:35 +0100

    styled invoice info component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import RequestInformation from './components/request/RequestInformation';
+import InvoiceInfo from './components/request/InvoiceInfo';
+// import RequestInformation from './components/request/RequestInformation';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <RequestInformation />
+      {/* <RequestInformation /> */}
+      <InvoiceInfo />
     </div>
   );
 }
---
+++ src/components/request/InvoiceInfo.js
@@ -0,0 +1,101 @@
+import React from "react";
+import "./InvoiceInfo.css";
+
+const InvoiceInfo = () => {
+  return (
+    <div className='invoice-info-container'>
+      <div className='request-info-title'>Request Information</div>
+      <div className='request-info-title'>Request Information</div>
+
+      <div className='invoice-form'>
+        <form>
+          <div className='invoice-form-one'>
+            <div className='form-col'>
+              <label>Customer/Company name</label>
+              <input type='text' name='name' placeholder='Spleet Limited' />
+            </div>
+
+            <div className='mid-col'>
+              <div className='form-col'>
+                <label>Email address</label>
+                <input type='text' name='name' placeholder='info@spleet.ng' />
+              </div>
+              <div className='form-col'>
+                <label>Phone number</label>
+                <input type='text' name='name' placeholder='08062540732' />
+              </div>
+            </div>
+
+            <div className='form-col'>
+              <label>Home/ Building Address</label>
+              <input
+                type='text'
+                name='name'
+                placeholder='51, Iwaya Road, Onike, Iwaya'
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className='invoice-form-two'>
+              <div className='mid-col'>
+                <div className='form-col'>
+                  <label>Issue date</label>
+                  <input type='text' name='name' placeholder='08/10/2020' />
+                </div>
+                <div className='form-col'>
+                  <label>Due date</label>
+                  <input type='text' name='name' placeholder='08/10/2020' />
+                </div>
+              </div>
+
+              <div className='mid-col'>
+                <div className='form-col'>
+                  <label>Invoice number</label>
+                  <input type='text' name='name' placeholder='00000001' />
+                </div>
+                <div className='form-col'>
+                  <label>V.A.T %</label>
+                  <input type='text' name='name' placeholder='7.5%' />
+                </div>
+              </div>
+
+              <div className='form-col'>
+                <label>Service description</label>
+                <input
+                  type='text'
+                  name='name'
+                  placeholder='5 Beedroom Duplex post-con cleaning'
+                />
+              </div>
+
+              <div className='mid-col'>
+                <div className='form-col'>
+                  <label>Service amount</label>
+                  <input type='text' name='name' placeholder='NGN 45,000' />
+                </div>
+                <div className='form-col amount'>
+                  <label>Total amount</label>
+                  <div className='field'>
+                    <p>NGN 48,375</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className='mid-col'>
+              <div className='form-col cancel'>
+                <input type='submit' value='CANCEL' />
+              </div>
+              <div className='form-col create'>
+                <input type='submit' value='CREATE INVOICE' />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default InvoiceInfo;

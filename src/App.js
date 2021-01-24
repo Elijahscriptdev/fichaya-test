e2657b51bceaa014f9794c6aa1bc4fe4b1e1dac7
Elijahscriptdev <elijahobominuru@gmail.com>
2021-01-24 11:12:08 +0100

    arrange component structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import Navbar from './components/navbar/Navbar';
-import PreviewInvoice from './components/request/PreviewInvoice';
+// import Navbar from './components/navbar/Navbar';
+// import PreviewInvoice from './components/request/PreviewInvoice';
 // import InvoiceInfo from './components/request/InvoiceInfo';
-// import RequestInformation from './components/request/RequestInformation';
+import RequestInformation from './components/request/RequestInformation';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      {/* <RequestInformation /> */}
+      {/* <Navbar /> */}
+      <RequestInformation />
       {/* <InvoiceInfo /> */}
-      <PreviewInvoice />
+      {/* <PreviewInvoice /> */}
     </div>
   );
 }
